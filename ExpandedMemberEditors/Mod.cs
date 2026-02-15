ba1ae1e65340b78a76923340c35b262b1683abb6
Nytra <14206961+Nytra@users.noreply.github.com>
2026-02-15 08:39:00 +0000

    Dict values won't be null

--- ExpandedMemberEditors/Mod.cs
+++ ExpandedMemberEditors/Mod.cs
@@ -112,15 +112,7 @@ public class ExpandedMemberEditors : ResoniteMod
 				ui.VerticalLayout(4f);
 				foreach (var kvp in dict.BoxedEntries)
 				{
-					if (kvp.Value is not null)
-					{
-						SyncMemberEditorBuilder.Build(kvp.Value, kvp.Key.ToString()!, null!, ui, labelSize);
-					}
-					else
-					{
-						ui.Style.MinHeight = 24f;
-						ui.Text($"{kvp.Key}: <null>");
-					}
+					SyncMemberEditorBuilder.Build(kvp.Value, kvp.Key.ToString()!, null!, ui, labelSize);
 				}
 				ui.NestOut();
 				ui.NestOut();
