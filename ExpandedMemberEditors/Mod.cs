669f213224ab08354c10877ac0048886347571cb
Nytra <14206961+Nytra@users.noreply.github.com>
2026-08-29 20:49:41 +0100

    Better dictionary UI

--- ExpandedMemberEditors/Mod.cs
+++ ExpandedMemberEditors/Mod.cs
@@ -53,6 +53,126 @@ public class ExpandedMemberEditors : ResoniteMod
 	}
 #endif
 
+	[HarmonyPatch(typeof(SyncMemberEditorBuilder), "BuildDictionary")]
+	class ExpandedMemberEditorsPatch2
+	{
+		static void BuildDictionaryElements(ISyncDictionary dict, UIBuilder ui, float labelSize)
+		{
+			foreach (var kvp in dict.BoxedEntries)
+			{
+				ui.PushStyle();
+				ui.Style.MinHeight = 24f;
+				ui.Style.FlexibleWidth = 1f;
+				ui.HorizontalLayout(4f);
+				SyncMemberEditorBuilder.Build(kvp.Value, kvp.Key.ToString()!, null!, ui, labelSize);
+				ui.PushStyle();
+				ui.Style.FlexibleWidth = -1f;
+				ui.Button("Remove").LocalPressed += (btn, data) =>
+				{
+					dict.RemoveByKey(kvp.Key);
+				};
+				ui.PopStyle();
+				ui.NestOut();
+				ui.PopStyle();
+			}
+		}
+		static bool Prefix(ISyncDictionary dictionary, string name, FieldInfo fieldInfo, UIBuilder ui, float labelSize)
+		{
+			if (!config!.GetValue(Key_Enabled)) return true;
+
+			ui.PushStyle();
+			ui.Style.MinHeight = -1f;
+
+			ui.VerticalLayout(4f);
+			ui.Style.MinHeight = 24f;
+			var text = ui.Text((LocaleString)(name + " (dictionary):"), bestFit: true, null, parseRTF: false);
+			colorX color33 = dictionary.GetType().GetTypeColor().MulRGB(1.5f);
+			InteractionElement.ColorDriver colorDriver = text.Slot.AttachComponent<Button>().ColorDrivers.Add();
+			colorDriver.ColorDrive.Target = text.Color;
+			colorDriver.NormalColor.Value = MathX.LerpUnclamped(RadiantUI_Constants.TEXT_COLOR, in color33, 0.1f);
+			colorDriver.HighlightColor.Value = RadiantUI_Constants.LABEL_COLOR;
+			colorDriver.PressColor.Value = RadiantUI_Constants.HEADING_COLOR;
+			text.Slot.AttachComponent<ReferenceProxySource>().Reference.Target = dictionary;
+			ui.Style.MinHeight = -1f;
+
+			ui.VerticalLayout(4f);
+			var listRoot = ui.Root;
+			BuildDictionaryElements(dictionary, ui, labelSize);
+			dictionary.Changed += (changeable) =>
+			{
+				if (listRoot.FilterWorldElement() is null) return;
+				listRoot.DestroyChildren();
+				ui.NestInto(listRoot);
+				BuildDictionaryElements(dictionary, ui, labelSize);
+			};
+			ui.NestOut();
+
+			ui.PushStyle();
+			ui.Style.MinHeight = 24f;
+			ui.Style.FlexibleWidth = 1f;
+
+			ui.HorizontalLayout(4f);
+
+			ui.PushStyle();
+			ui.Style.FlexibleWidth = -1f;
+			ui.Text("Key:");
+			ui.PopStyle();
+
+			ui.PushStyle();
+			ui.Style.MinWidth = 120f;
+			var keyComp = ui.CurrentRect.Slot.AttachComponent(typeof(ValueField<>).MakeGenericType(dictionary.KeyType));
+			ui.PrimitiveMemberEditor((IField)keyComp.GetSyncMember("Value"));
+			ui.PopStyle();
+
+			var valueType = dictionary.GetType().GetGenericArguments()[1];
+			if (valueType.IsEnginePrimitive())
+			{
+				ui.PushStyle();
+				ui.Style.FlexibleWidth = -1f;
+				ui.Text("Value:");
+				ui.PopStyle();
+
+				ui.PushStyle();
+				ui.Style.MinWidth = 240f;
+				var valueComp = ui.CurrentRect.Slot.AttachComponent(typeof(ValueField<>).MakeGenericType(valueType));
+				ui.PrimitiveMemberEditor((IField)valueComp.GetSyncMember("Value"));
+				ui.PopStyle();
+
+				ui.Button("Add").LocalPressed += (btn, data) =>
+				{
+					var member = dictionary.Add(((IField)keyComp.GetSyncMember("Value")).BoxedValue);
+					var thing = (IField)member;
+					thing.BoxedValue = ((IField)valueComp.GetSyncMember("Value")).BoxedValue;
+				};
+			}
+			else
+			{
+				ui.PushStyle();
+				ui.Style.FlexibleWidth = -1f;
+				ui.Text("Reference:");
+				ui.PopStyle();
+
+				ui.PushStyle();
+				ui.Style.MinWidth = 240f;
+				var refComp = ui.CurrentRect.Slot.AttachComponent(typeof(ReferenceField<>).MakeGenericType(valueType));
+				ui.RefMemberEditor((ISyncRef)refComp.GetSyncMember("Reference"));
+				ui.PopStyle();
+
+				ui.Button("Add").LocalPressed += (btn, data) =>
+				{
+					var member = dictionary.Add(((IField)keyComp.GetSyncMember("Value")).BoxedValue);
+					var thing = (IField)member;
+					thing.BoxedValue = ((IField)refComp.GetSyncMember("Reference")).BoxedValue;
+				};
+			}
+			ui.PopStyle();
+			ui.NestOut();
+			ui.NestOut();
+			ui.PopStyle();
+			return false;
+		}
+	}
+
 	[HarmonyPatch(typeof(SyncMemberEditorBuilder), "Build")]
 	class ExpandedMemberEditorsPatch
 	{
@@ -64,6 +184,7 @@ public class ExpandedMemberEditors : ResoniteMod
 			{
 				ui.PushStyle();
 				ui.Style.MinHeight = -1f;
+				
 				ui.VerticalLayout(4f);
 
 				var genProxySource = config.GetValue(Key_VarProxySources);
@@ -91,30 +212,6 @@ public class ExpandedMemberEditors : ResoniteMod
 					ui.Text($"{(genProxySource ? "" : $"{name}: ")}<null>");
 				}
 
-				ui.NestOut();
-				ui.PopStyle();
-			}
-			else if (member is ISyncDictionary dict)
-			{
-				ui.PushStyle();
-				ui.Style.MinHeight = -1f;
-				ui.VerticalLayout(4f);
-				ui.Style.MinHeight = 24f;
-				var text = ui.Text((LocaleString)(name + " (dictionary):"), bestFit: true, null, parseRTF: false);
-				colorX color33 = dict.GetType().GetTypeColor().MulRGB(1.5f);
-				InteractionElement.ColorDriver colorDriver = text.Slot.AttachComponent<Button>().ColorDrivers.Add();
-				colorDriver.ColorDrive.Target = text.Color;
-				colorDriver.NormalColor.Value = MathX.LerpUnclamped(RadiantUI_Constants.TEXT_COLOR, in color33, 0.1f);
-				colorDriver.HighlightColor.Value = RadiantUI_Constants.LABEL_COLOR;
-				colorDriver.PressColor.Value = RadiantUI_Constants.HEADING_COLOR;
-				text.Slot.AttachComponent<ReferenceProxySource>().Reference.Target = dict;
-				ui.Style.MinHeight = -1f;
-				ui.VerticalLayout(4f);
-				foreach (var kvp in dict.BoxedEntries)
-				{
-					SyncMemberEditorBuilder.Build(kvp.Value, kvp.Key.ToString()!, null!, ui, labelSize);
-				}
-				ui.NestOut();
 				ui.NestOut();
 				ui.PopStyle();
 			}
